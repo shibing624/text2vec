9240e6d24454fa05fb97ec4af257e7f7efce7233
xuming06 <507153809@qq.com>
2019-11-12 22:42:02 +0800

    update usage.

--- tests/base.py
+++ tests/base.py
@@ -5,8 +5,16 @@
 """
 
 from simtext import Similarity
-sim  = Similarity()
+
+sim = Similarity()
 a = '湖北人爱吃鱼'
 b = '甘肃人不爱吃鱼'
+c = '武汉人'
 s = sim.score(a, b)
-print(s)
+print(a, b, s)
+
+s = sim.score(a, c)
+print(a, c, s)
+
+s = sim.score(b, c)
+print(b, c, s)
