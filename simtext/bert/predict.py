6750f242ea2daad4970b5b1a4b2e5922a2e715ad
xuming06 <507153809@qq.com>
2019-11-19 23:00:38 +0800

    add info.

--- simtext/bert/predict.py
+++ simtext/bert/predict.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
                          output_dir=os.path.join(simtext.USER_DATA_DIR, 'fine_tuned_bert_similarity'))
     sim.set_mode(tf.estimator.ModeKeys.PREDICT)
     while True:
+        print('input start:')
         sentence1 = input('sentence1: ')
         sentence2 = input('sentence2: ')
         predict = sim.predict(sentence1, sentence2)
