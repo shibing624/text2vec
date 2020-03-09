1406e8428468f1e633e36cf53d9dadaddd0e1ad9
xuming624 <xuming624@qq.com>
2020-03-09 11:27:46 +0800

    fix bug:https://github.com/shibing624/text2vec/issues/8

---
+++ tests/issue_test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing（xuming624@qq.com)
+@description: 
+"""
+
+from text2vec import Similarity
+
+sim = Similarity()
+
+
+def test_sim_diff():
+    a = '研究团队面向国家重大战略需求追踪国际前沿发展借鉴国际人工智能研究领域的科研模式有效整合创新资源解决复'
+    b = '英汉互译比较语言学'
+
+    print(a, b, sim.get_score(a, b))
+
+
+def test_sim_same():
+    a = '汉英翻译比较语言学'
+    b = '英汉互译比较语言学'
+
+    print(a, b, sim.get_score(a, b))
