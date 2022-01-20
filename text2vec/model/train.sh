68b29abcfaa0e746f41f4772bf095ab8281bcf9e
shibing624 <shibing624@126.com>
2022-01-20 22:02:39 +0800

    update train shell.

--- text2vec/model/train.sh
+++ text2vec/model/train.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=0
 mkdir -p outputs
-python train.py --task_name STS-B --output_dir outputs/STS-B-model > outputs/STS-B.log
-python train.py --task_name ATEC --output_dir outputs/ATEC-model > outputs/ATEC.log
-python train.py --task_name BQ --output_dir outputs/BQ-model > outputs/BQ.log
-python train.py --task_name LCQMC --output_dir outputs/LCQMC-model > outputs/LCQMC.log
-python train.py --task_name PAWSX --output_dir outputs/PAWSX-model > outputs/PAWSX.log
+python train.py --task_name STS-B --output_dir outputs/STS-B-model > outputs/STS-B.log 2>&1
+python train.py --task_name ATEC --output_dir outputs/ATEC-model > outputs/ATEC.log 2>&1
+python train.py --task_name BQ --output_dir outputs/BQ-model > outputs/BQ.log 2>&1
+python train.py --task_name LCQMC --output_dir outputs/LCQMC-model > outputs/LCQMC.log 2>&1
+python train.py --task_name PAWSX --output_dir outputs/PAWSX-model > outputs/PAWSX.log 2>&1
