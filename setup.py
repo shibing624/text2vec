ca96e65f6cdd7590f0edd29f6cb0f3ad10558127
shibing624 <shibing624@126.com>
2022-02-23 20:43:08 +0800

    update setup.

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
-
 import sys
 
 from setuptools import setup, find_packages
@@ -29,22 +27,25 @@ setup(
     url='https://github.com/shibing624/text2vec',
     license="Apache License 2.0",
     zip_safe=False,
-    python_requires='>=3.5',
+    python_requires=">=3.6.0",
     classifiers=[
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Natural Language :: Chinese (Simplified)',
-        'Natural Language :: Chinese (Traditional)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Text Processing',
-        'Topic :: Text Processing :: Indexing',
-        'Topic :: Text Processing :: Linguistic',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     keywords='word embedding,text2vec,Chinese Text Similarity Calculation Tool,similarity,word2vec',
     install_requires=reqs.strip().split('\n'),
     packages=find_packages(exclude=['tests']),
     package_dir={'text2vec': 'text2vec'},
-    package_data={'text2vec': ['*.*', '../LICENSE', '../README.*', '../*.txt', 'utils/*',
+    package_data={'text2vec': ['*.*', 'utils/*',
                                'data/*', 'data/STS-B/*', 'data/English-STS-B/stsbenchmark.tsv.gz', ]}
 )
