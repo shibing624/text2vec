0fa45d6dcb4abfd4f0af0c8ae480479a5afca9d1
shibing624 <shibing624@126.com>
2023-09-14 22:05:59 +0800

    update cli

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     license="Apache License 2.0",
     zip_safe=False,
     python_requires=">=3.6.0",
-    entry_points={"console_scripts": ["text2vec = text2vec.cli"]},
+    entry_points={"console_scripts": ["text2vec = text2vec.cli:main"]},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
--- text2vec/version.py
+++ text2vec/version.py
@@ -4,4 +4,4 @@
 @description:
 """
 
-__version__ = '1.2.5'
+__version__ = '1.2.6'
