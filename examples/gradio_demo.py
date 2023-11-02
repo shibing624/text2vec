f37c0da97182cd0c482dddd169e2618a8b965060
shibing624 <shibing624@126.com>
2023-11-02 20:26:58 +0800

    fix gradio https://github.com/shibing624/text2vec/issues/136

--- examples/gradio_demo.py
+++ examples/gradio_demo.py
@@ -5,6 +5,7 @@
 """
 
 import gradio as gr
+
 from text2vec import Similarity, EncoderType
 
 # 中文句向量模型(CoSENT)
@@ -26,16 +27,13 @@ if __name__ == '__main__':
         ['一个女人在看书。', '一个女人在揉面团'],
         ['一个男人在车库里举重。', '一个人在举重。'],
     ]
-    input1 = gr.inputs.Textbox(lines=2, placeholder="Enter First Sentence")
-    input2 = gr.inputs.Textbox(lines=2, placeholder="Enter Second Sentence")
-
-    output_text = gr.outputs.Textbox()
-    gr.Interface(ai_text,
-                 inputs=[input1, input2],
-                 outputs=[output_text],
-                 # theme="grass",
-                 title="Chinese Text to Vector Model shibing624/text2vec-base-chinese",
-                 description="Copy or input Chinese text here. Submit and the machine will calculate the cosine score.",
-                 article="Link to <a href='https://github.com/shibing624/text2vec' style='color:blue;' target='_blank\'>Github REPO</a>",
-                 examples=examples
-                 ).launch()
+    gr.Interface(
+        ai_text,
+        inputs=['text', 'text'],
+        outputs='text',
+        theme="soft",
+        title="Chinese Text to Vector Model shibing624/text2vec-base-chinese",
+        description="Copy or input Chinese text here. Submit and the machine will calculate the cosine score.",
+        article="Link to <a href='https://github.com/shibing624/text2vec' style='color:blue;' target='_blank\'>Github REPO</a>",
+        examples=examples
+    ).launch()
